194b6a6cc59155f9127a96a8d56f61fb625f8d67
TomaszLapa <tomasz.marcin.lapa@gmail.com>
2023-06-09 09:38:58 +0200

    project pivot

--- src/js/menu.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav');
-
-const handleClick = () => {
-    hamburger.classList.toggle('hamburger--active');
-    nav.classList.toggle('nav--active');
-}
-
-hamburger.addEventListener('click', handleClick);
